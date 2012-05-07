7932ab8078587d390dbd7dd353c4ed8919ac129f
bowsinger <bowsinger@thinkreals.com>
2012-05-07 12:23:09 +0900

    deploy 변경

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,50 +1,49 @@
-$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
+set :default_stage, "production"
+set :stages, %w(production staging)
+require 'capistrano/ext/multistage'
+require 'bundler/capistrano'
 
-require "rvm/capistrano"
-require "bundler/capistrano"
+set :use_sudo, false
+set :scm, :git
+set :repository, "git@github.com:bowsinger/nubo.git"
+set :ssh_options, {:forward_agent=>true}
+set :deploy_via, :remote_cache
+# default shared_children is %w(public/system log tmp/pids)
+set :shared_children, fetch(:shared_children) + ['tmp/sockets', 'vendor/bundle']
+set :copy_exclude, ['.git']
 
-set :rvm_type, :user  # Don't use system-wide RVM
-
-# be sure to change these
 default_run_options[:pty] = true
 
-set :user, 'darmmg'
-set :domain, 'nu-bo.com'
-set :application, 'nu-bo'
-# file paths
-set :repository, "svn://vps108.narru.net:40037/nubo/trunk"
-set :deploy_to, "/home/darammg/deploy/#{application}"
-# distribute your applications across servers (the instructions below put them
-# all on the same server, defined above as 'domain', adjust as necessary)
-role :app, domain
-role :web, domain
-role :db, domain, :primary => true
-
-# miscellaneous options
-set :use_sudo, false
-set :scm, 'subversion'
-
-def remote_file_exists?(full_path)
-  'true' ==  capture("if [ -e #{full_path} ]; then echo 'true'; fi").strip
-end
-
 namespace :deploy do
-  task :restart do
-    #run "ln -s #{deploy_to}/#{shared_dir}/public/images/ #{current_release}/assets/images/photos/", {:roles => :app}
-    run "cd #{current_release}; RAILS_ENV=production bundle exec rake assets:precompile"
-    if remote_file_exists?("#{current_release}/tmp/pids/unicorn.pid")
-      run "kill -USR2 `cat #{current_release}/tmp/pids/unicorn.pid`"
-    else
-      run "cd #{current_path} && bundle exec unicorn -c #{current_path}/config/unicorn.rb -E #{fetch(:rails_env, "production")} -D"
-    end  
+  desc "use environment configuration"
+  task :copy_env_config, :roles => [:app, :web] do
+    config_dir = rails_env unless exists?(:config_d)
+    run "cp -rf #{current_release}/config.d/#{config_dir}/* #{current_release}/config/"
   end
+  
+  desc "add priviledge to executables"
+  task :chmod_executables, :roles => [:app, :web] do
+    run <<-EOS
+      chmod u+x #{current_release}/script/* &&
+      chmod u+x #{current_release}/lib/tasks/cb_namecheck64 &&
+      chmod u+x #{current_release}/lib/surem/smscall &&
+      chmod u+x #{current_release}/lib/jobs/*.sh &&
+      chmod u+x #{current_release}/lib/feeds/*.sh
+    EOS
+  end
+  task :start, :roles => :app do
+    run "cd #{current_path}; script/unicorn start"
+  end
+  
+  task :stop, :roles => :app do
+    run "cd #{current_path}; script/unicorn stop"
+  end
+  
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "cd #{current_path}; script/unicorn upgrade"
+  end
+    
 end
 
-namespace :bundler do
-  task :symlink_bundled_gems, :roles => :app do
-    run "ln -nfs #{shared_path}/bundle #{release_path}/vendor/bundle"
-  end
-end
-
-after 'deploy:update_code', 'bundler:symlink_bundled_gems'
-
+after "deploy:update", "deploy:copy_env_config"
+after "deploy:update", "deploy:chmod_executables"
